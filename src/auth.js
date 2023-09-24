fec1497f4c20897788e7532a4bae8ed8111b2350
DennisBaksheev <dennis.baksheev8@gmail.com>
2023-09-24 14:39:08 -0400

    Remove src/auth.js

--- src/auth.js
+++ src/auth.js
@@ -1,5 +1,10 @@
 // src/auth.js
 
+// We expect AWS_COGNITO_POOL_ID and AWS_COGNITO_CLIENT_ID to be defined.
+if (!(process.env.AWS_COGNITO_POOL_ID && process.env.AWS_COGNITO_CLIENT_ID)) {
+  throw new Error('missing expected env vars: AWS_COGNITO_POOL_ID, AWS_COGNITO_CLIENT_ID');
+}
+
 const passport = require('passport');
 const BearerStrategy = require('passport-http-bearer').Strategy;
 const { CognitoJwtVerifier } = require('aws-jwt-verify');
