f916033323e068a200472e15ae03f3fb3a967a58
DennisBaksheev <dennis.baksheev8@gmail.com>
2023-09-25 23:49:26 -0400

    Refactor to use response.js functions

--- src/app.js
+++ src/app.js
@@ -12,12 +12,13 @@ const { strategy, authenticate } = require('./auth/cognito');
 
 const { author, version } = require('../package.json');
 const logger = require('./logger');
-console.log('Author:', author);
-console.log('Version:', version);
 const pino = require('pino-http')({
   // Use our default logger instance, which is already configured
   logger,
 });
+
+// Import the response handling functions
+const { createSuccessResponse, createErrorResponse } = require('./response');
 
 // Create an express app instance we can use to attach middleware and HTTP routes
 const app = express();
@@ -42,26 +43,20 @@ app.use(passport.initialize());
 // Create a router that we can use to mount our API
 const router = express.Router();
 
-/**
- * Expose all of our API routes on /v1/* to include an API version.
- * Protect them all so you have to be authenticated in order to access.
- */
+// Expose all of our API routes on /v1/* to include an API version.
+// Protect them all so you have to be authenticated in order to access.
 router.use('/v1', authenticate(), require('./routes/api'));
 
-/**
- * Define a simple health check route. If the server is running
- * we'll respond with a 200 OK.  If not, the server isn't healthy.
- */
+// Define a simple health check route.
 router.get('/', (req, res) => {
-  // Client's shouldn't cache this response (always request it fresh)
   res.setHeader('Cache-Control', 'no-cache');
-  // Send a 200 'OK' response
-  res.status(200).json({
-    status: 'ok',
-    author,
-    githubUrl: 'https://github.com/DennisBaksheev/fragments',
-    version,
-  });
+  res.status(200).json(
+    createSuccessResponse({
+      author,
+      githubUrl: 'https://github.com/DennisBaksheev/fragments',
+      version,
+    })
+  );
 });
 
 // Define our routes
@@ -69,35 +64,20 @@ app.use('/', router);
 
 // Add 404 middleware to handle any requests for resources that can't be found
 app.use((req, res) => {
-  res.status(404).json({
-    status: 'error',
-    error: {
-      message: 'not found',
-      code: 404,
-    },
-  });
+  res.status(404).json(createErrorResponse(404, 'not found'));
 });
 
 // Add error-handling middleware to deal with anything else
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
-  // We may already have an error response we can use, but if not,
-  // use a generic `500` server error and message.
   const status = err.status || 500;
   const message = err.message || 'unable to process request';
 
-  // If this is a server error, log something so we can see what's going on.
   if (status > 499) {
     logger.error({ err }, `Error processing request`);
   }
 
-  res.status(status).json({
-    status: 'error',
-    error: {
-      message,
-      code: status,
-    },
-  });
+  res.status(status).json(createErrorResponse(status, message));
 });
 
 // Export our `app` so we can access it in server.js
