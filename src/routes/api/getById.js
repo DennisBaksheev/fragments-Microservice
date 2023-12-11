1e261c353efb050b4b2729ee3eaee56978a670aa
Dennis Baksheev <113218159+DennisBaksheev@users.noreply.github.com>
2023-12-10 19:46:21 -0500

    Added type conversion in project

--- src/routes/api/getById.js
+++ src/routes/api/getById.js
@@ -1,9 +1,63 @@
 const crypto = require('crypto');
 const { Fragment } = require('../../Model/fragment');
-// Use https://www.npmjs.com/package/content-type to create/parse Content-Type headers
-// const contentType = require('content-type');
-// const createSuccessResponse = require('../../response').createSuccessResponse;
+const md = require('markdown-it')();
+const { htmlToText } = require('html-to-text');
+const sharp = require('sharp');
 const createErrorResponse = require('../../response').createErrorResponse;
+
+const getExtensionContentType = (extension) => {
+  switch (extension) {
+    case 'txt':
+      return 'text/plain';
+    case 'md':
+      return 'text/markdown';
+    case 'html':
+      return 'text/html';
+    case 'json':
+      return 'application/json';
+    case 'png':
+      return 'image/png';
+    case 'jpg':
+      return 'image/jpeg';
+    case 'webp':
+      return 'image/webp';
+    case 'gif':
+      return 'image/gif';
+    default:
+      return null;
+  }
+};
+
+const convertData = async (data, from, to) => {
+  let convertedData = data;
+  switch (from) {
+    case 'text/markdown':
+      if (to === 'txt') {
+        convertedData = md.render(data.toString());
+        convertedData = htmlToText(convertedData);
+      } else if (to === 'html') {
+        convertedData = md.render(data.toString());
+      }
+      break;
+    case 'text/html':
+      if (to === 'txt') {
+        convertedData = htmlToText(data.toString());
+      }
+      break;
+    case 'application/json':
+      if (to === 'txt') {
+        convertedData = JSON.stringify(data);
+      }
+      break;
+    case 'image/png':
+    case 'image/jpeg':
+    case 'image/webp':
+    case 'image/gif':
+      convertedData = await sharp(data).toFormat(to.split('/')[1]).toBuffer();
+      break;
+  }
+  return convertedData;
+};
 
 function validConversion(contentType, extension) {
   let formats = [];
@@ -21,92 +75,61 @@ function validConversion(contentType, extension) {
       formats = ['.json', '.txt'];
       break;
     case 'image/png':
-      formats = ['.png', '.jpg', '.webp', '.gif'];
-      break;
     case 'image/jpeg':
-      formats = ['.png', '.jpg', '.webp', '.gif'];
-      break;
     case 'image/webp':
-      formats = ['.png', '.jpg', '.webp', '.gif'];
-      break;
     case 'image/gif':
       formats = ['.png', '.jpg', '.webp', '.gif'];
       break;
     default:
       return false;
   }
-
-  const includeExt = (element) => element.includes(extension);
-
-  return formats.some(includeExt);
+  return formats.some((element) => element.includes('.' + extension));
 }
 
 module.exports = async (req, res) => {
-  // await Fragment.byId('1234'))
   const idWithExt = req.params.id;
   let user = crypto.createHash('sha256').update(req.user).digest('hex');
-
   const idWithExtArray = idWithExt.split('.');
-
-  let id;
-  let ext;
+  let id, ext;
 
   if (idWithExtArray.length > 1) {
-    id = idWithExtArray.slice(0, idWithExtArray.length - 1).join('.');
-    ext = idWithExtArray[idWithExtArray.length - 1];
+    id = idWithExtArray[0];
+    ext = idWithExtArray[1];
   } else {
     id = idWithExtArray[0];
     ext = null;
   }
 
   const idList = await Fragment.byUser(user);
-
-  if (idList.includes(id)) {
-    const fragmentObject = await Fragment.byId(user, id);
-    let fragment;
-
-    if (fragmentObject instanceof Fragment) {
-      fragment = fragmentObject;
-    } else {
-      fragment = new Fragment({
-        id: id,
-        ownerId: fragmentObject.ownerId,
-        created: fragmentObject.created,
-        update: fragmentObject.update,
-        type: fragmentObject.type,
-        size: fragmentObject.size,
-      });
-    }
-
-    if (fragment) {
-      let dataResult = null;
-
-      if (!ext) {
-        dataResult = await fragment.getData();
-      } else {
-        if (validConversion(fragment.mimeType, ext)) {
-          dataResult = await fragment.getData();
-        }
-      }
-
-      if (dataResult) {
-        res.setHeader('Content-Type', fragment.mimeType);
-        res.status(200).send(dataResult);
-      } else {
-        const error = 'Cannot convert from ' + fragment.mimeType + ' to ' + ext;
-        createErrorResponse(
-          res.status(415).json({
-            code: 415,
-            message: error,
-          })
-        );
-      }
-    }
-  } else {
-    createErrorResponse(
+  if (!idList.includes(id)) {
+    return createErrorResponse(
       res.status(404).json({
-        message: 'Not found id!',
+        message: 'Fragment not found!',
       })
     );
+  }
+
+  const fragmentObject = await Fragment.byId(user, id);
+  let fragment = fragmentObject instanceof Fragment ? fragmentObject : new Fragment(fragmentObject);
+  let dataResult = await fragment.getData();
+
+  if (ext) {
+    if (validConversion(fragment.mimeType, ext)) {
+      const extensionType = getExtensionContentType(ext);
+      const convertedData = await convertData(dataResult, fragment.mimeType, extensionType);
+
+      res.setHeader('Content-Type', extensionType);
+      res.status(200).send(convertedData);
+    } else {
+      // Handle unsupported conversion
+      return createErrorResponse(
+        res.status(415).json({
+          message: 'Unsupported conversion',
+        })
+      );
+    }
+  } else {
+    res.setHeader('Content-Type', fragment.mimeType);
+    res.status(200).send(dataResult);
   }
 };
